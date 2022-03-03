ec8869f06639fd55d8559001aaa19686e483deb7
asif10388 <asif.khan10388@gmail.com>
2022-03-03 16:15:25 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className={`h-screen flex justify-center items-center`}>
+      <h1 className="text-blue-600 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
